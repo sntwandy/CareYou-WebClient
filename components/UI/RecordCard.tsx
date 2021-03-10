214d39d8140bcfbe99c1bd8c0659b52ebd04775f
Jose Domingo <jose.dom215@gmail.com>
2021-03-09 22:57:58 -0400

    add part of the icons

--- components/UI/RecordCard.tsx
+++ components/UI/RecordCard.tsx
@@ -19,22 +19,26 @@ const RecordCard = (props: IProps): ReactElement => {
   const { record } = props;
   return (
     <div className={'flex flex-wrap items-center justify-center bg-black text-white font-extralight text-sm rounded-tl-input rounded-br-input p-3'} style={{color: '#fff'}}>
-      <div className="flex" style={{ width: '100%' }}>
-        <div className="flex flex-1" style={{}}>
-          <span className="mr-2">📅</span>
+      <div className={'flex'} style={{ width: '100%' }}>
+        <div className={'flex flex-1'} style={{}}>
+          <span className="mr-1">
+            <img src={'/calendar.png'} alt={'Calendar Icon'} />
+          </span>
           {format(record.date, 'yyyy/MM/dd')}
         </div>
-        <div className="flex flex-1 flex-row-reverse" style={{ textAlign: 'end' }}>
+        <div className={'flex flex-1 flex-row-reverse'} style={{ textAlign: 'end' }}>
           <span>✅</span>
         </div>
       </div>
-      <div className="flex" style={{ width: '100%' }}>
-        <div className="flex flex-1" style={{display: 'block-inline'}}>
-          <span className="mr-2">👨🏻‍⚕️</span>
+      <div className={'flex'} style={{ width: '100%' }}>
+        <div className={'flex flex-1 font-light'} style={{display: 'block-inline'}}>
+          <span className={'mr-2'}>👨🏻‍⚕️</span>
           {record.attendant}
         </div>
-        <div className="flex flex-1">
-          <span className="mr-2">📄</span>
+        <div className={'flex flex-1'}>
+          <span className={'mr-2'}>
+            <img src={'/notepad.png'} alt={'Notepad Icon'} />
+          </span>
           {record.case}
         </div>
       </div>
