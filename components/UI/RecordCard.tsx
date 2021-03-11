889f98b5cc90429895fdef4bcba658309ec6b76e
Jose Domingo <jose.dom215@gmail.com>
2021-03-10 20:25:32 -0400

    add missing icons

--- components/UI/RecordCard.tsx
+++ components/UI/RecordCard.tsx
@@ -18,26 +18,26 @@ interface IProps {
 const RecordCard = (props: IProps): ReactElement => {
   const { record } = props;
   return (
-    <div className={'flex flex-wrap items-center justify-center bg-black text-white font-extralight text-sm rounded-tl-input rounded-br-input p-3'} style={{color: '#fff'}}>
-      <div className={'flex'} style={{ width: '100%' }}>
+    <div className={'flex flex-wrap items-center justify-center bg-black text-white font-light text-sm rounded-tl-input rounded-br-input p-3'} style={{color: '#fff'}}>
+      <div className={'flex mb-2'} style={{ width: '100%' }}>
         <div className={'flex flex-1'} style={{}}>
           <span className="mr-1">
-            <img src={'/calendar.png'} alt={'Calendar Icon'} />
+            <img src={'https://i.imgur.com/8D02AvD.png'} alt={'Calendar Icon'} />
           </span>
           {format(record.date, 'yyyy/MM/dd')}
         </div>
         <div className={'flex flex-1 flex-row-reverse'} style={{ textAlign: 'end' }}>
-          <span>✅</span>
+          <img src={'https://i.imgur.com/0xEUuBg.png'} alt={'Calendar Icon'} />
         </div>
       </div>
       <div className={'flex'} style={{ width: '100%' }}>
-        <div className={'flex flex-1 font-light'} style={{display: 'block-inline'}}>
-          <span className={'mr-2'}>👨🏻‍⚕️</span>
+        <div className={'flex flex-1 font-normal'}>
+          <span className={'mr-1'}><img src={'https://i.imgur.com/6GOiFv8.png'} alt={'Calendar Icon'} /></span>
           {record.attendant}
         </div>
         <div className={'flex flex-1'}>
-          <span className={'mr-2'}>
-            <img src={'/notepad.png'} alt={'Notepad Icon'} />
+          <span className={'mr-1'}>
+            <img src={'https://i.imgur.com/TxO70I1.png'} alt={'Notepad Icon'} />
           </span>
           {record.case}
         </div>
