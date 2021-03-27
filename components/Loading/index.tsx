7f208318cb23e30e4d98cd0e43699170384d6527
Wandy Santana <sntwandy@gmail.com>
2021-03-27 06:32:25 -0400

    Adding a loading component, almost finished

---
+++ components/Loading/index.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className={'w-full h-full flex items-center justify-center'}>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
--- utils/Auth/index.tsx
+++ utils/Auth/index.tsx
@@ -5,17 +5,19 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import JWT from '../jwt';
+import Loading from '../../components/Loading';
 
 const Auth = (props: any) => {
 
   /* Initializations */
   const router = useRouter();
+  const [isLogged, setIsLogged] = useState(false);
 
   /* Component Update */
   useEffect(() => {
     const token = localStorage.getItem('Token')
     if (token) {
-      !JWT(token) && router.push('/Login')
+      !JWT(token) ? router.push('/Login') : setIsLogged(true);
     } else {
       router.push('/Login')
     }
@@ -23,7 +25,7 @@ const Auth = (props: any) => {
 
   return (
     <>
-      {props.children}
+    {isLogged ? props.children : <Loading /> }
     </>
   );
 };
