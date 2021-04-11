8052179bd0a0e9be77b05e128c602b9bb0703c96
Jose Domingo <jose.dom215@gmail.com>
2021-04-10 21:16:46 -0400

    Add form validation

--- components/Buttons/PrimaryButton.tsx
+++ components/Buttons/PrimaryButton.tsx
@@ -11,16 +11,17 @@ interface IProps {
   inverted?: boolean;
   middle?: boolean;
   full?: boolean;
+  type?: string;
 }
 
 const input: FC<IProps> = (props: IProps):ReactElement => {
   const colorClasses = () => (inverted ? 'bg-secondary text-primary hover:text-secondaryButtonHover border-button border-primary' : 'bg-primary text-secondary hover:text-primaryButtonHover');
-  const { label, onClick, disabled = false, inverted = false, middle = false, full = false} = props;
+  const { label, onClick, disabled = false, inverted = false, middle = false, full = false, type = 'button'} = props;
 
   return (
     <input
       className={`${middle ? 'w-buttonMiddle' : 'w-primaryButton'} ${full ? 'w-primaryInput' : 'w-primaryButton'} h-primaryButton ${colorClasses()} text-lg font-normal rounded-button focus:outline-none`}
-      type={'button'}
+      type={type}
       value={label}
       disabled={disabled}
       onClick={() => onClick()}
--- pages/SingUp/index.tsx
+++ pages/SingUp/index.tsx
@@ -4,58 +4,73 @@
 
 import React, { FC, ReactElement, useState } from 'react';
 import Link from 'next/link';
-
-// Hooks
-import useInput from '../../hooks/useInput';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 // Components
 import Input from '../../components/Inputs/PrimaryInput';
 import Button from '../../components/Buttons/PrimaryButton';
 import Title from '../../components/Titles/Title';
 
+interface IFormSchema {
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    gender: string;
+    country: string;
+    province: string;
+    postalCode: string;
+    addressLine: string;
+    suffering: string;
+    idCard: string;
+    userName: string;
+    email: string;
+    password: string;
+}
+const onlyLettersRgx: RegExp = /^[a-zA-Z]+$/;
+const onlyNumbersRgx: RegExp = /^[0-9]+$/;
+const alphanumericRgx: RegExp = /^[a-zA-Z0-9]+$/;
+const formSchema = yup.object().shape({
+  firstName: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }).required(),
+  lastName: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }).required(),
+  birthDate: yup.string(),
+  gender: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }),
+  country: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }),
+  province: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }),
+  postalCode: yup.string().matches(onlyNumbersRgx, { excludeEmptyString: true }),
+  addressLine: yup.string(),
+  suffering: yup.string().matches(onlyLettersRgx, { excludeEmptyString: true }),
+  idCard: yup.string().matches(onlyNumbersRgx, { excludeEmptyString: true }),
+  userName: yup.string().matches(alphanumericRgx, { excludeEmptyString: true }),
+  email: yup.string().email().required(),
+  password: yup.string().min(6),
+});
+
 // SingUp
 const SingUp: FC = (): ReactElement => {
-
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormSchema>({
+    mode: 'all',
+    resolver: yupResolver(formSchema)
+  });
   // Form steps and progress
-  const progress: string[]= ['0%', '33%', '66%', '90%', '100%'];
+  const progress: string[] = ['0%', '33%', '66%', '90%', '100%'];
   const [step, setStep] = useState(1);
 
-  // User data to login, useInput hook
-  const { value: firstName, bind: bindFirst } = useInput('');
-  const { value: lastName, bind: bindLast } = useInput('');
-  const { value: birthDate, bind: bindBirthDate } = useInput('');
-  const { value: gender, bind: bindGender } = useInput('');
-  const { value: country, bind: bindCountry } = useInput('');
-  const { value: province, bind: bindProvince } = useInput('');
-  const { value: postalCode, bind: bindPostalCode } = useInput('');
-  const { value: addressLine, bind: bindAddressLine } = useInput('');
-  const { value: suffering, bind: bindSuffering } = useInput('');
-  const { value: idCard, bind: bindIdCard } = useInput('');
-  const { value: userName, bind: bindUserName } = useInput('');
-  const { value: email, bind: bindEmail } = useInput('');
-  const { value: password, bind: bindPassword } = useInput('');
-
   // Handling submit button
-  const handleSubmit = (): void => {
-    console.log({
-      firstName,
-      lastName,
-      birthDate,
-      gender,
-      country,
-      province,
-      postalCode,
-      addressLine,
-      suffering,
-      idCard,
-      userName,
-      email,
-      password,
-    });
-  };
+  const onSubmit = handleSubmit(data => {
+    if (step <= 4) {
+      setStep(step + 1)
+    }
+    console.log('onSubmit', data)
+  });
+  const ifValidationErrors: boolean = !!Object.keys(errors).length;
+
+  // TODO: Use a stylized component here to render fancy errors
+  const _renderError = (field: keyof IFormSchema) => errors[field]?.message;
 
   return (
-    <div className={'flex items-center justify-center flex-col w-full h-full'}>
+    <form className={'flex items-center justify-center flex-col w-full h-full'} onSubmit={onSubmit}>
       {/* LOGO AND TITLE */}
       <div className={'flex items-center justify-center flex-col mb-9'}>
         <figure className={'w-20 h-20 mb-6'}>
@@ -81,18 +96,26 @@ const SingUp: FC = (): ReactElement => {
       <div className={'flex items-center justify-center flex-col'}>
         {step === 1 && (
           <div>
-            <Input label={'First Name'} placeholder={'Zeus'} {...bindFirst} required />
-            <Input label={'Last Name'} placeholder={'Amenadiel'} {...bindLast} required />
-            <Input label={'Birth Date'} placeholder={'17-09-1995'} {...bindBirthDate} type={'date'} required />
-            <Input label={'Gender'} placeholder={'Gender'} {...bindGender} required />
+            <Input label={'First Name'} placeholder={'Zeus'} bind={register('firstName')} required />
+            {_renderError('firstName')}
+            <Input label={'Last Name'} placeholder={'Amenadiel'} bind={register('lastName')} required />
+            {_renderError('lastName')}
+            <Input label={'Birth Date'} placeholder={'17-09-1995'} bind={register('birthDate')} type={'date'} required />
+            {_renderError('birthDate')}
+            <Input label={'Gender'} placeholder={'Gender'} bind={register('gender')} required />
+            {_renderError('gender')}
           </div>
         )}
         {step === 2 && (
           <div>
-            <Input label="Country" placeholder="Dominican Republic" {...bindCountry} required />
-            <Input label="Province" placeholder="Santo Domingo" {...bindProvince} required />
-            <Input label="Postal Code" placeholder="41000" {...bindPostalCode} required />
-            <Input label="Address Lines" placeholder="Autopista Duarte, #17" {...bindAddressLine} required />
+            <Input label="Country" placeholder="Dominican Republic" bind={register('country')} required />
+            {_renderError('country')}
+            <Input label="Province" placeholder="Santo Domingo" bind={register('province')} required />
+            {_renderError('province')}
+            <Input label="Postal Code" placeholder="41000" bind={register('postalCode')} required />
+            {_renderError('postalCode')}
+            <Input label="Address Lines" placeholder="Autopista Duarte, #17" bind={register('addressLine')} required />
+            {_renderError('addressLine')}
           </div>
         )}
         {step === 3 && (
@@ -104,15 +127,20 @@ const SingUp: FC = (): ReactElement => {
                 </p>
               </div>
             </div>
-            <Input label="Suffering" placeholder="Tachycardia" {...bindSuffering} required />
+            <Input label="Suffering" placeholder="Tachycardia" bind={register('suffering')} required />
+            {_renderError('suffering')}
           </div>
         )}
         {step === 4 && (
           <div>
-            <Input label="ID Card" placeholder="402-2392919-1" {...bindIdCard} required />
-            <Input label="User Name" placeholder="zeus_amen" {...bindUserName} required />
-            <Input label="Email" placeholder="zamenadiel@email.com" {...bindEmail} required />
-            <Input label="Password" placeholder="sec2re-pass4rd" {...bindPassword} type="password" required />
+            <Input label="ID Card" placeholder="402-2392919-1" bind={register('idCard')} required />
+            {_renderError('idCard')}
+            <Input label="User Name" placeholder="zeus_amen" bind={register('userName')} required />
+            {_renderError('userName')}
+            <Input label="Email" placeholder="zamenadiel@email.com" bind={register('email')} required />
+            {_renderError('email')}
+            <Input label="Password" placeholder="sec2re-pass4rd" bind={register('password')} type="password" required />
+            {_renderError('password')}
           </div>
         )}
         {step === 5 && (
@@ -125,30 +153,27 @@ const SingUp: FC = (): ReactElement => {
           {step > 1 && step <= 3 && (
             <div className={'mt-4 flex items-center justify-center flex-rows space-x-2'}>
               <Button onClick={() => setStep(step - 1)} middle={true} label="Back" inverted />
-              <Button onClick={() => setStep(step + 1)} middle={true} label="Next" />
+              <Button onClick={() => setStep(step + 1)} middle={true} label="Next" disabled={ifValidationErrors} />
             </div>
-            )}
+          )}
           {step === 1 ? (
             <div className={'mt-4'}>
-              <Button onClick={() => setStep(step + 1)} label="Next" />
+              <Button onClick={() => setStep(step + 1)} label="Next" disabled={ifValidationErrors} />
             </div>
           ) : step === 4 && (
             <div className={'mt-4 flex items-center justify-center flex-rows space-x-2'}>
               <Button onClick={() => setStep(step - 1)} middle={true} label="Back" inverted />
               <Button onClick={() => {
-                if (step <= 4) {
-                  setStep(step + 1)
-                }
-                handleSubmit()
-              }} middle={true} label="Finish" />
+                onSubmit()
+              }} middle={true} label="Finish" type='submit' disabled={ifValidationErrors} />
             </div>
           )}
         </div>
       </div>
-        <span className="text-sm font-light mt-8">
-          <Link href="/Login">You already have an account?</Link>
-        </span>
-    </div>
+      <span className="text-sm font-light mt-8">
+        <Link href="/Login">You already have an account?</Link>
+      </span>
+    </form>
   );
 };
 
