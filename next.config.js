b87a54f5db30f70a8fa31b5a924af12d48f62468
Wandy Santana <sntwandy@gmail.com>
2021-06-14 01:25:03 -0400

    nextconfig enabled

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+module.exports = {
+  env: {
+    SECRET_KEY:
+      '()TL6*}O2b;lz?F1)C<~G+,5;JbIk+%f[DSh3t*7U%g:nP.f?gsPp7e{6jRY0,/~nFAK{@Uu:|^+=76C;tO*uf%er:9',
+    BACKEND_URL: 'https://ec2-34-217-45-231.us-west-2.compute.amazonaws.com/',
+    GET_USER_URL:
+      'https://ec2-34-217-45-231.us-west-2.compute.amazonaws.com/users/info',
+    LOGIN_USERS_URL:
+      'https://ec2-34-217-45-231.us-west-2.compute.amazonaws.com/users/login',
+    SINGUP_USERS_URL:
+      'https://ec2-34-217-45-231.us-west-2.compute.amazonaws.com/users/singup',
+    DIAGNOSIS_URL:
+      'https://ec2-34-217-45-231.us-west-2.compute.amazonaws.com/diagnosis',
+  },
+};
