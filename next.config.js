92bf1b1a98cd7ae43236a007f1f81d310bcf625e
sntwandy <sntwandy@gmail.com>
2021-07-10 05:37:21 -0400

    Updating the api url

--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ module.exports = {
       '()TL6*}O2b;lz?F1)C<~G+,5;JbIk+%f[DSh3t*7U%g:nP.f?gsPp7e{6jRY0,/~nFAK{@Uu:|^+=76C;tO*uf%er:9',
     BACKEND_URL: 'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/',
     GET_USER_URL:
-      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/users/info',
+      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/api/users/info',
     LOGIN_USERS_URL:
-      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/users/login',
+      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/api/users/login',
     SINGUP_USERS_URL:
-      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/users/singup',
+      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/api/users/singup',
     DIAGNOSIS_URL:
-      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/diagnosis',
+      'http://ec2-34-216-78-233.us-west-2.compute.amazonaws.com/api/diagnosis',
   },
 };
